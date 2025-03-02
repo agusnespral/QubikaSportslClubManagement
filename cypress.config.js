cb46e0c0483d0ffb2f9798dc7cdbea524c3e156c
Agustin <agusnespral@gmail.com>
2025-03-02 18:35:54 -0300

    installed package.json and its dependencies, intalles cypress and created framework, created .gitignore file, added script to mute logs, defined api and base url

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    hideXHRInCommandLog: true,
+
+    env: {
+      apiUrl: "https://api.club-administration.qa.qubika.com/api",
+      baseUrl: "https://club-administration.qa.qubika.com/#/auth/login",
+    }
+  },
+});
