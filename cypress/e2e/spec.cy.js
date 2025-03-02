020adf741cceb298e00c7777ab034a8c5bdef6e4
Agustin <agusnespral@gmail.com>
2025-03-02 19:28:28 -0300

    created a fixture. created on test file before to register a new user

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,29 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('Technical test - Excercise 3', function () {
+
+
+  /* before('Register a new user trough API and save user information', function () {
+    
+    cy.fixture('data.json').then((data) => {
+      this.data = data;
+
+      cy.request({
+        method: 'POST',
+        url: `${Cypress.env().apiUrl}/auth/register`,
+
+        body: {
+          "email": this.data.email,
+          "password": this.data.password,
+          "roles": [
+            this.data.roles
+          ]
+        }
+      })
+    });
+  }); */
+
+  it('', () => {
+    cy.visit(Cypress.env().baseUrl)
+  });
+
+  
 })
